c853c042dc0719cb7374a4a9e2a5f27deb318144
IKISSA PENDY Sevan Kedesh <pendysevan10@gmail.com>
2023-10-07 15:04:26 +0100

    API of Cutlture ministry

---
+++ src/api/acteur/controllers/acteur.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * acteur controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::acteur.acteur');
---
+++ src/api/acteur/services/acteur.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * acteur service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::acteur.acteur');
